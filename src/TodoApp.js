605ea58bce59c03f51a7470d9819f53a8179598a
Lukuok <aaaacin@naver.com>
2020-03-19 17:57:08 +0900

    update

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,6 +4,24 @@ import Form from './TodoComponents/Form';
 import TodoItemList from './TodoComponents/TodoItemList'
 
 class TodoApp extends Component {
+
+    id = 3;
+
+    state = {
+        input: '',
+        todos: [
+            {id:0, text: ' 리액트 소개', checked: false},
+            {id:1, text: ' 리액트 소개', checked: true},
+            {id:2, text: ' 리액트 소개', checked: false}
+        ]
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            input: e.target.value
+        });
+    }
+
     render() {
         return (
             <TodoListTemplate form={<Form/>}>
