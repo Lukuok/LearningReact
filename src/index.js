9d7be7f62b512de37d3cea3bc3d66e3dd8a9ea23
Lukuok <aaaacin@naver.com>
2020-03-26 17:29:22 +0900

    update

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App     from './App';
 import Game    from './Game';
 import TodoApp from './TodoApp';
+import Board   from './board/BoardMain'
 import TodoApp2 from './TodoApp2';
 import * as serviceWorker from './serviceWorker';
 
 //ReactDOM.render(<App /> , document.getElementById('root'));
 //ReactDOM.render(<TodoApp />, document.getElementById('todoApp'));
-ReactDOM.render(<TodoApp2 />, document.getElementById('todoApp2'));
+//ReactDOM.render(<TodoApp2 />, document.getElementById('todoApp2'));
+ReactDOM.render(<Board />, document.getElementById('board'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
