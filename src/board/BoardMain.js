db288ebc12caf2474fa9e83c99119f30cbca4229
Lukuok <aaaacin@naver.com>
2020-03-27 14:36:16 +0900

    study update

--- src/board/BoardMain.js
+++ src/board/BoardMain.js
@@ -3,7 +3,7 @@ import {Route, BrowserRouter as Router } from 'react-router-dom';
 
 import App1 from './App1';
 import App2 from './App2';
-// import App3 from './App3';
+import App3 from './App3';
 // import App4 from './App4';
 // import App5 from './App5';
 // import App6 from './App6';
@@ -16,6 +16,7 @@ class BoardMain extends Component {
                     <Route exact path="/"     component={App1} />
                     <Route exact path="/App1" component={App1} />
                     <Route exact path="/App2" component={App2} />
+                    <Route exact path="/App3" component={App3} />
                 </div>
             </Router>
         );
